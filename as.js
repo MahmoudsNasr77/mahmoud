b16a0d7dd8b1e200aaf172b99392d1dbe0c4aa3e
MahmoudsNasr77 <67973033+MahmoudsNasr77@users.noreply.github.com>
2020-07-08 00:39:17 +0200

    Add files via upload

---
+++ as.js
@@ -0,0 +1,11 @@
+$(function () {
+    "use strict";
+  $("#button").click(function () {
+  $("p").show("slow", function () {$("header").hide("1400");
+   $("#p4").toggle("slow");
+   $("input").hide("slow");
+   $(".div6").hide("slow");
+   $("aside").fadeIn();});
+   });
+    
+});
